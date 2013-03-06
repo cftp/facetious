c378e08822c38fc187ce6d1df8c6518ebafea752
Simon Wheatley <simon@sweetinteraction.com>
2013-03-06 15:29:22 +0000

    Increment version.

--- facetious.php
+++ facetious.php
@@ -3,7 +3,7 @@
 Plugin Name:  Facetious Search
 Plugin URI:   https://github.com/cftp/facetious
 Description:  A faceted search interface for WordPress
-Version:      1.1.1
+Version:      1.1.2
 Author:       <a href="http://johnblackbourn.com/">John Blackbourn</a> and <a href="http://codeforthepeople.com/">Code for the People</a>
 Text Domain:  facetious
 Domain Path:  /languages/
