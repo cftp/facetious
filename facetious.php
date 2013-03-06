57ed926583ad96b3617230d1d82dd2393f840951
Simon Wheatley <simon@sweetinteraction.com>
2013-03-06 15:33:52 +0000

    Reppin’ hard.

--- facetious.php
+++ facetious.php
@@ -11,6 +11,23 @@ License:      GPL v2 or later
 
 Copyright © 2012 John Blackbourn / Code for the People ltd
 
+                _____________
+               /      ____   \
+         _____/       \   \   \
+        /\    \        \___\   \
+       /  \    \                \
+      /   /    /          _______\
+     /   /    /          \       /
+    /   /    /            \     /
+    \   \    \ _____    ___\   /
+     \   \    /\    \  /       \
+      \   \  /  \____\/    _____\
+       \   \/        /    /    / \
+        \           /____/    /___\
+         \                        /
+          \______________________/
+
+
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
